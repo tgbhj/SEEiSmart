fa5624b53972991cc1faa3363601562c279145c2
tgbhj <j1060381781@qq.com>
2020-06-03 15:01:38 +0800

    commit

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
     httpPort: process.env.httpPort || 80,
     //httpPort: process.env.httpPort || 4500,
+    // mongoUser: process.env.mongoUser || 'smart',
+    // mongoPass: process.env.mongoPass || 'smart123',
     mongoHost: process.env.mongoHost || '127.0.0.1',
     // mongoHost: process.env.mongoHost || '192.168.1.112',
     mongoPort: process.env.mongoPort || 27017,
-    // mongoUser: process.env.mongoUser || 'smart',
-    // mongoPass: process.env.mongoPass || 'smart123',
     mongoDataBase: process.env.mongoDataBase || 'adinno'
 }
